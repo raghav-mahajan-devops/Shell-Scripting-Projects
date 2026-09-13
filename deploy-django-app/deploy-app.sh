27d5e9acb090eb009d3fc5e447824555f382ae19
raghav-mahajan-devops <raghav.mahajan.devops@gmail.com>
2026-09-13 18:12:37 +0000

    added deploy code

--- deploy-django-app/deploy-app.sh
+++ deploy-django-app/deploy-app.sh
@@ -12,15 +12,15 @@ function clone_repo {
 	
 	cloned=$(find /home -type d -name "django-notes-app" 2>/dev/null)
 	if [ ! -n "$cloned" ];then
-		echo "start cloning the app ..."
+		echo -e "******************************* start cloning the app ****************************** \n"
 		git clone https://github.com/LondheShubham153/django-notes-app.git
 		if [ $? -eq 0 ];then
-			echo "App is cloned successfully"
+			echo -e "*********************** App is cloned successfully ************************* \n"
 		else
-			echo "App is not cloned sum failures occur"
+			echo -e "*********************** App is not cloned sum failures occur *************** \n"
 		fi
 	else
-		echo "App is already cloned."
+		echo -e "******************************* App is already cloned ****************************** \n"
 	fi
 }
 
@@ -30,36 +30,59 @@ function install_requirements {
 	docker_install=$(which docker.io)
 	nginx_install=$(which nginx)
 	compose_install=$(which docker-compose)
+	echo -e "**************************************** updating the apt-get ******************************** \n" 
 	sudo apt-get update
 	if [ -z "$docker_install" ];then
-		echo "installing the docker ..."
+		echo -e "******************************** installing the docker ******************************* \n"
 		sudo apt-get install -y docker.io
 	else
-		echo "Docker is already installed"
+		echo -e "******************************** Docker is already installed ************************* \n"
 
 	fi
 
 	if [ -z "$nginx_install" ];then
-                echo "installing the nginx ..."
+                echo -e "******************************** installing the nginx ******************************** \n"
                 sudo apt-get install -y nginx
         else
-                echo "Nginx is already installed"
+                echo -e "******************************** Nginx is already installed ************************** \n"
 
         fi
 
 	if [ -z "$compose_install" ];then
-                echo "installing the docker composer ..."
+                echo -e "******************************* installing the docker composer *********************** \n"
                 sudo apt-get install -y docker-compose
         else
-                echo "Docker composer is already installed"
+                echo -e "******************************* Docker composer is already installed ***************** \n"
 
         fi
 
 }
 
+function restart_services {
+	echo -e "*************************************** Changing the ownership for docker.sock *************** \n"
+	sudo chown $USER /var/run/docker.sock
+	sudo systemctl enable docker
+	sudo systemctl enable nginx
+}
+
+function deploy_app {
+	cd django-notes-app
+	echo "****************************************** Listing the folders in the repo ********************** \n"
+	ls -l 
+	echo -e "*************************************** building and deploying the app *********************** \n"
+	docker build -t notesapp .
+	docker run -d -p 8000:8000 notesapp:latest
+	echo -e "*************************************** App build is successfully **************************** \n"
+}	
+
+
+
 function run_script {
+
 clone_repo
 install_requirements
+restart_services
+deploy_app
 }
 
 run_script
